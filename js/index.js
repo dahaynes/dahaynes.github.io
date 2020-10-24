bcfb29863ac66af21035579c363d62d9deeb833c
dahaynes <dahaynes@umn.edu>
2020-10-23 23:09:28 -0500

    website

--- js/index.js
+++ js/index.js
@@ -1,20 +1,27 @@
 function loadMetaData() {
     console.log("Loaded");
-    data = $.getJSON("C:\git\dahaynes.github.io\js\articles-metadata.json"), function(data)  {
+/*     data = $.getJSON("C:\git\dahaynes.github.io\js\articles-metadata.json"), function(data)  {
     var items = [];
     /* $.each( data, function( key, val ) {
         items.push( "<li id='" + key + "'>" + val + "</li>" );
-      }); */
+      }); 
     $.each(data, function(i, field) {
         console.log(field)
         }) 
-    }
+    } */
 };
+
 
 
 $(document).ready(function () {
     
-    loadMetaData()
+    loadMetaData();
+/*     $.getJSON("https://gist.github.com/dahaynes/a944c39f21c97fcd8d92c9eff7afc4c2.js", function (data) {
+        console.log(data.name); // Prints: Harry
+        console.log(data.age); // Prints: 14
+    }).fail(function () {
+        console.log("An error has occurred.");
+    }); */
 
     
 });
